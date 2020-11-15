b9221815adc14f0df7cd72241ff21961eb94d5b9
alessio rea <alexei.80@hotmail.fr>
2020-11-15 19:40:12 +0100

    cleaning + config

--- src/config/config.py
+++ src/config/config.py
@@ -1,13 +1,45 @@
 import os
 NAME_FILE = 'data.csv'
-FULL_PATH_DATA = os.path.os.getcwd()
+FULL_PATH_DATA = os.path.os.getcwd()   
 
 REPO_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
 DATA_DIR = os.path.join(REPO_DIR, 'data')
 FILE_DATA = os.path.join(DATA_DIR, NAME_FILE)
 
 OUTPUTS_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../output'))
-SAVED_FILENAME = 'processed_data.csv'
+OUTPUTS_MODELS_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../models'))
+
+
+_SAVED_FILENAME = '_processed_data.csv'
+JL = '.joblib'
+
+LOG_REG_SAVED_FILENAME = 'log_reg' + _SAVED_FILENAME
+LOG_REG_MODEL_FILE = 'log_reg' + JL
+
+KNN = 'knn'
+KNN_SAVED_FILENAME = KNN + _SAVED_FILENAME
+KNN_MODEL_FILE = KNN + JL
+
+RF = 'rf'
+RF_SAVED_FILENAME = RF + _SAVED_FILENAME
+RF_MODEL_FILE = RF + JL
+
+SVC = 'svc'
+SVC_SAVED_FILENAME = SVC + _SAVED_FILENAME
+SVC_MODEL_FILE = SVC + JL
+
+GB = 'gb'
+GB_SAVED_FILENAME = GB + _SAVED_FILENAME
+GB_MODEL_FILE = GB + JL
+
+
+INFO_LOG_FILE_NAME = 'lead_scoring_info_log.log'
+DATA_DIR = os.path.join(REPO_DIR, 'logs')
+FILE_LOG = os.path.join(DATA_DIR, INFO_LOG_FILE_NAME)
+LOGGING_FORMAT = '[%(asctime)s][%(levelname)s][%(module)s] - %(message)s'
+LOGGING_DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
+#FORMAT = '%(asctime)-15s %(clientip)s %(user)-8s %(message)s'
+
 
 #new column names :
 ID_CLIENT = 'ID_CLIENT'
@@ -45,6 +77,9 @@ SOUH_REC_MAJ_PROG = 'SOUH_REC_MAJ_PROG'
 SOUH_REC_MAJ_MP = 'SOUH_REC_MAJ_MP'
 SOUH_PAYER_CHEQUE = 'SOUH_PAYER_CHEQUE'
 SOUH_REC_COPIE_LB = 'SOUH_REC_COPIE_LB'
+
+PRED_COL_NAME = 'PREDICTION'
+PRED_PROBA_COL_NAME = 'PREDICTED_PROBABILITY'
  
 
 NEW_COL_NAMES = [
@@ -85,88 +120,118 @@ NEW_COL_NAMES = [
     SOUH_REC_COPIE_LB
 ]
 
+
+import re
+
+def clean_line(line) :
+    e = re.compile('[éèê]')
+    a = re.compile('[àâ]')
+    u = re.compile('[û]')
+    o = re.compile('[ô]')
+
+    line = e.sub('e', line)
+    line = a.sub('a', line)
+    line = u.sub('u', line)
+    line = o.sub('o', line)
+    return line.lower().rstrip().lstrip()
+
+
+
+#############################
+
+
+use_origin_lead = []
+path =os.path.abspath(os.path.join(os.path.dirname(__file__), 'use_origin_lead.txt'))
+f = open(path, 'r') 
+lines = f.readlines()
+for line in lines:
+    line = clean_line(line)
+    use_origin_lead.append(line)
+
+
+use_source_lead = []
+path =os.path.abspath(os.path.join(os.path.dirname(__file__), 'use_source_lead.txt'))
+f = open(path, 'r') 
+lines = f.readlines()
+for line in lines: 
+    line = clean_line(line)
+    use_source_lead.append(line)
+
+
+use_niveau_lead = []
+path =os.path.abspath(os.path.join(os.path.dirname(__file__), 'use_niveau_lead.txt'))
+f = open(path, 'r') 
+lines = f.readlines()
+for line in lines: 
+    line = clean_line(line)
+    use_niveau_lead.append(line)
+
+
+use_qualite_lead = []
+path =os.path.abspath(os.path.join(os.path.dirname(__file__), 'use_qualite_lead.txt'))
+f = open(path, 'r') 
+lines = f.readlines()
+for line in lines: 
+    line = clean_line(line)
+    use_qualite_lead.append(line)
+
+
+use_der_act_not = []
+path =os.path.abspath(os.path.join(os.path.dirname(__file__), 'use_der_act_not.txt'))
+f = open(path, 'r') 
+lines = f.readlines()
+for line in lines: 
+    line = clean_line(line)
+    use_der_act_not.append(line)
+
+
+use_tags = []
+path =os.path.abspath(os.path.join(os.path.dirname(__file__), 'use_tags.txt'))
+f = open(path, 'r') 
+lines = f.readlines()
+for line in lines: 
+    line = clean_line(line)
+    use_tags.append(line)
+
+
+use_ville = []
+path =os.path.abspath(os.path.join(os.path.dirname(__file__), 'use_ville.txt'))
+f = open(path, 'r') 
+lines = f.readlines()
+for line in lines: 
+    line = clean_line(line)
+    use_ville.append(line)
+
+
+use_spec = []
+path =os.path.abspath(os.path.join(os.path.dirname(__file__), 'use_spec.txt'))
+f = open(path, 'r') 
+lines = f.readlines()
+for line in lines: 
+    line = clean_line(line)
+    use_spec.append(line)
+
+
+use_index_act = []
+path =os.path.abspath(os.path.join(os.path.dirname(__file__), 'use_index_act.txt'))
+f = open(path, 'r') 
+lines = f.readlines()
+for line in lines: 
+    line = clean_line(line)
+    use_index_act.append(line)
+
+
+
 CAT_FEAT = [
     ORIGINE_LEAD, SOURCE_LEAD, NIVEAU_LEAD, TAGS, VILLE, SPECIALISATION, INDEX_ACTIVITE,
     QUALITE_LEAD, CONTACT_PAR_MAIL, STATUT_ACTUEL, DERNIERE_ACTIVITE, DERNIERE_ACTIVITE_NOTABLE
 ]
 
 
-
-CAT_FEAT_TO_PROCESS = [
-    ORIGINE_LEAD, SOURCE_LEAD, NIVEAU_LEAD, TAGS, VILLE, SPECIALISATION, INDEX_ACTIVITE, QUALITE_LEAD, DERNIERE_ACTIVITE_NOTABLE
+CAT_FEAT = [
+    ORIGINE_LEAD, SOURCE_LEAD, NIVEAU_LEAD, TAGS, VILLE, SPECIALISATION, DERNIERE_ACTIVITE_NOTABLE
 ]
 
-
-NUM_FEAT = [ NB_VISITES, SCORE_ACTIVITE, NB_PAGES_VUES_PAR_VISITE, SCORE_PROFIL, DUREE_SUR_SITEWEB ]
-
-
-COLS_TO_KEEP = CAT_FEAT + NUM_FEAT + [TARGET]
-
-
-use_origin_lead = ['formulaire lead add', 'soumission landing page', 'api']
-
-use_source_lead = ['olark chat', 'organic search', 'google', 'reference',
-                    'direct traffic', 'facebook', 'referral sites', 'welingak website']
-
-use_niveau_lead = ['select', 'autre leads', 'lead potentiel', "etudiant d'une certaine ecole"]
-
-use_qualite_lead = ['pas du tout pertinent', 'pourrait etre pertinent', 'pas sur',
-                    'tres pertinent', 'peu pertinent']
-
-use_der_act_not = ['email ouvert', 'modifie', 'sms envoye', 'page visitee sur le site', 'conversation chat',
-                    'a clique sur le lien dans le mail', 'email rejete', 'desinscrit', 'email marque comme spam']
-
-use_tags = ['appele', 'diplome en cours', 'deja un etudiant', 'desactive', 'ferme', "interesse par d'autres cours",
-            'interesse par un mba full-time', 'occupe', "perdu au profit d'un concurrent",
-        'ne pas suivre de formation continue', 'reviendra apres avoir lu le courriel']
-
-use_ville = ['select', 'autres villes de maharashtra', 'mumbai', 'autres villes', 
-                'thane et sa peripherie', 'autres villes metropolitaines']
-
-use_spec = ['marketing management', 'banking, investment and insurance',
-       'hospitality management', 'finance management', 'select',
-       'it projects management', 'human resource management',
-       'travel and tourism', 'supply chain management',
-       'operations management', 'international business',
-       'business administration', 'healthcare management',
-       'media and advertising', 'retail management']
-
-use_index_act = ['moyen', 'eleve', 'faible']
-
-#############################
-
-
-use_origin_lead = ['Formulaire Lead Add', 'Soumission landing page', 'API']
-
-use_source_lead = ['Olark Chat', 'Organic Search', 'Google', 'Reference', 'Direct Traffic', 
-                   'Facebook', 'Referral Sites', 'Welingak Website']
-
-use_niveau_lead = ['Select', 'Autre leads', 'Lead potentiel', "Etudiant d'une certaine école"]
-
-use_qualite_lead = ['Pas du tout pertinent', 'Pourrait être pertinent', 'Pas sur', 
-                    'Très pertinent', 'Peu pertinent']
-
-use_der_act_not = ['Email ouvert', 'Modifié', 'SMS envoyé', 'Page visitée sur le site', 
-                   'Conversation Chat', 'A cliqué sur le lien dans le mail', 'Email rejeté',
-                   'Désinscrit', 'Email marqué comme Spam']
-
-use_tags = ['Appelé', 'Diplôme en cours', 'Déjà un étudiant', 'Désactivé', 'Fermé', "Intéressé par d'autres cours", 
- 'Intéressé par un MBA full-time',  'Occupé', "Perdu au profit d'un concurrent", 
- 'Ne pas suivre de formation continue', 'Reviendra après avoir lu le courriel']
-
-use_ville = ['Select', 'Autres villes de Maharashtra', 'Mumbai', 'Autres villes', 'Thane et sa périphérie',
-               'Autres villes métropolitaines']
-
-use_spec = ['Marketing Management', 'Banking, Investment And Insurance',
-       'Hospitality Management', 'Finance Management', 'Select',
-       'IT Projects Management',
-       'Human Resource Management', 'Travel and Tourism',
-       'Supply Chain Management', 'Operations Management',
-       'International Business', 'Business Administration',
-       'Healthcare Management', 'Media and Advertising',
-       'Retail Management']
-
-use_index_act = ['Moyen', 'Elevé', 'Faible']
 
 use_lists = [
     use_origin_lead,
@@ -175,12 +240,27 @@ use_lists = [
     use_tags,
     use_ville,
     use_spec,
-    use_index_act,
-    use_qualite_lead,
-    use_der_act_not,
+    use_der_act_not
 ]
 
 
+CAT_FEAT_ORD = [
+    INDEX_ACTIVITE, QUALITE_LEAD
+]
 
+
+use_lists_ord = [
+    use_index_act,
+    use_qualite_lead
+]
+
+
+NUM_FEAT = [ NB_VISITES, SCORE_ACTIVITE, NB_PAGES_VUES_PAR_VISITE, SCORE_PROFIL, DUREE_SUR_SITEWEB ]
+
+
+FEATURES = CAT_FEAT + CAT_FEAT_ORD + NUM_FEAT
+
+
+COLS_TO_KEEP = FEATURES + [TARGET]
 
 
