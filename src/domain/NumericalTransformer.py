05ec4a21076ca6c874574bf9184e66c17f95b866
Marieme GUEYE <masogu724@gmail.com>
2020-11-15 20:39:35 +0100

    add numerical tranformer

--- src/domain/NumericalTransformer.py
+++ src/domain/NumericalTransformer.py
@@ -1,16 +1,22 @@
 from sklearn.base import BaseEstimator, TransformerMixin
 
-
 #Custom transformer we wrote to engineer features 
 class NumericalTransformer(BaseEstimator, TransformerMixin):
+    
     #Class Constructor
-    def __init__( self, param ):
-        self.param = param
+    def __init__( self ) :
+        pass
         
     #Return self, nothing else to do here
-    def fit( self, X, y = None ):
+    def fit( self, X, y = None ) :
         return self 
-    
-    #Custom transform method we wrote that creates aformentioned features and drops redundant ones 
-    def transform(self, X, y = None):
-        return X[]
+
+    #Custom transform that replace ouliers by the maximum value 
+    def transform(self, X , y = None ):
+        new_X =X
+
+        #remove outliers
+        new_X.loc[new_X ['NB_VISITES'] > 50,['NB_VISITES']] = 50 
+        new_X.loc[new_X ['NB_PAGES_VUES_PAR_VISITE'] > 20, ['NB_PAGES_VUES_PAR_VISITE']] = 20
+
+        return new_X 
